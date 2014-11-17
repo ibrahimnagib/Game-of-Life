9768ea75b94aba09f30ba91651ecd4cfdf902ce0
ibrahimnagib <ibrahim.nagib@utexas.edu>
2014-11-17 09:10:06 -0600

    intitializing TestLife.py

---
+++ TestLife.py
@@ -0,0 +1,41 @@
+ # TestLife.py
+# Ibrahim Nagib
+# In 2422
+
+# -------
+# imports
+# -------
+
+from io import StringIO
+from unittest import main, TestCase
+
+from Life import *
+
+# -----------
+# Test Life
+# -----------
+
+class TestLife (TestCase):
+
+    # -------
+    # AbstractCell
+    # -------
+
+    # -------
+    # ConwayCell
+    # -------
+
+    # -------
+    # FredkinCell
+    # -------
+
+    # -------
+    # Life
+    # -------
+
+    # -------
+    # WallCell
+    # -------
+
+
+    
