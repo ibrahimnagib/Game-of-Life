23ccc77d2381de8384276d88beb7f35f5f266546
ibrahimnagib <ibrahim.nagib@utexas.edu>
2014-11-20 21:31:06 -0600

    Update RunLife.py
    RunLife.py working for all with correct output except for Fredkin

--- RunLife.py
+++ RunLife.py
@@ -11,6 +11,7 @@
 # -------
 
 import sys
+from trialife import *
 
 # ---------------------
 # Life ConwayCell 21x13
@@ -22,6 +23,10 @@ Simulate 12 evolutions.
 Print every grid (i.e. 0, 1, 2, 3, ... 12)
 """
 
+game = Life("RunLife.in.txt")
+game.read_board()
+game.run(1,1)
+
 # ---------------------
 # Life ConwayCell 20x29
 # ---------------------
@@ -31,6 +36,9 @@ print("*** Life ConwayCell 20x29 ***")
 Simulate 28 evolutions.
 Print every 4th grid (i.e. 0, 4, 8, ... 28)
 """
+#game = Life()
+# game.read_board()
+# game.run(4,4)
 
 # ----------------------
 # Life ConwayCell 109x69
@@ -46,7 +54,12 @@ Print the 323rd grid.
 Simulate 2177 evolutions.
 Print the 2500th grid.
 """
-
+# game.read_board()
+# game.run(10,1)
+# game.run(283,283)
+# game.run(323,323)
+# game.run(2500,2500)
+# game.run(283,283)
 # ----------------------
 # Life FredkinCell 20x20
 # ----------------------
@@ -56,3 +69,5 @@ print("*** Life FredkinCell 20x20 ****")
 Simulate 5 evolutions.
 Print every grid (i.e. 0, 1, 2, ..., 5)
 """
+# game.read_board()
+# game.runF(5,1)
