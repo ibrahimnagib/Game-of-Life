9b643cfda5e8fcbd80bad387e8902a97fbd6a024
ibrahimnagib <ibrahim.nagib@utexas.edu>
2014-11-21 00:48:54 -0600

    Fully Working

--- Life.py
+++ Life.py
@@ -19,7 +19,7 @@ class FredkinCell(AbstractCell):
    def __init__(self, symbol):
        AbstractCell.__init__(self,symbol)
        self.name = "fredkin"
-       # self.status = 1
+       self.status = 1
        if symbol == "-":
           self.status = 0
           self.age = 0
@@ -30,7 +30,7 @@ class FredkinCell(AbstractCell):
           self.status = 1
           self.age = int(symbol)
 
-       self.live_neighbors = 0
+       #self.live_neighbors = 0
 
 
 class WallCell(AbstractCell):
@@ -64,37 +64,37 @@ class Life:
                 elif x[j] == "*":
                     cell = ConwayCell(x[j])
                     self.population += 1
-                else:
+                elif x[j] == "-":
                     cell = FredkinCell(x[j])
+                elif x[j] == "0":
+                    cell = FredkinCell(x[j])
+                    self.population += 1
+                # else:
+                #     cell = FredkinCell(x[j])
                 self.board[i+1][j+1] = cell
         self.infile.readline()       
         print("Generation = 0 ", "Population = ",self.population)
         self.show_board()
+        print()
 
 
     def play_round_FredkinCell(self):
       """
       Play...
       """
-      # i = 0
-      # j = 0
-
-      # for x in self.boards:
-      #     for f in x:
-      #         j += 1
-      #         if f.status == 1:
-      #             for m in range(i-1,i+1):
-      #                 for n in range(j-1,j+1):
-      #                     self.board[m][n].live_neighbors += 1
-      #     i += 1
       for x in self.board:
         for f in x:
           f.live_neighbors = 0
-      self.population = 0
+      #self.population = 0
       for i in range(1, self.cols - 1):
            for j in range(1, self.rows - 1):
                status = self.board[i][j].status
-               self.population += status
+               for m in range(i-1 , i +2):
+                   self.board[m][j].live_neighbors += status
+               for n in range(j-1 , j +2):
+                   self.board[i][n].live_neighbors += status
+               #self.board[m][n].live_neighbors += status
+               #self.population += status
 
       self.board[i][j].live_neighbors -= status
 
@@ -131,22 +131,24 @@ class Life:
             if gen % shown == 0:
               print("Generation = ",gen , "Population = ", self.population)
               self.show_board()
-              print
+              print()
             rounds -=1
             gen += 1
 
     def runF(self, rounds, shown):
-        gen = 0
-        print("Generation = ", gen, "Population = ",self.population)
-        self.show_board()
-        gen += 1
+        # gen = 0
+        # print("Generation = ", gen, "Population = ",self.population)
+        # self.show_board()
+        gen = 1
         while rounds > 0:
+            self.population = 0
             self.play_round_FredkinCell()
             self.update_board()
-            print
+            #print
             if gen % shown == 0:
               print("Generation = ",gen , "Population = ", self.population)
               self.show_board()
+              print()
             rounds -=1
             gen += 1
 
@@ -164,8 +166,11 @@ class Life:
                             f.status = 1
                             self.population += 1
                     elif f.name == "fredkin":
-                        if f.live_neighbors == 0 or f.live_neighbors == 2 or f.live_neighbors == 4:
-                            f.status =1
+                        if f.live_neighbors == 1 or f.live_neighbors == 3 :
+                            #print "lives"
+                            f.status = 1
+                            #f.age += 1
+                            f.symbol = str(f.age)
                             f.age += 1
                             self.population += 1
                     else:
@@ -188,9 +193,14 @@ class Life:
 
                     elif f.name == "fredkin":
                         if f.live_neighbors == 1 or f.live_neighbors == 3:
-                            f.age += 1
+                            #print("lives")
                             f.status = 1
+                            f.symbol = str(f.age)
+                            #f.age += 1
                             self.population += 1
+                        else:
+                            f.status = 0
+                            f.symbol = "-"
 
 
 
@@ -206,7 +216,7 @@ class Life:
         #     print(' '.join(str(x.live_neighbors) for x in s))
             
         for s in self.board[1:-1]:
-            print(' '.join(x.symbol for x in s[1:-1]))
+            print(''.join(x.symbol for x in s[1:-1]))
 
 
 
